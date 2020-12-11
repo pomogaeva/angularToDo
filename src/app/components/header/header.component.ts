9b03bcb1d246d8ac309022e78beb6177130514c6
Tetiana Pomohaieva <t.pomogaeva@gmail.com>
2020-12-11 20:19:58 +0200

    added header comp

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  @Input() testHeaderTitle: string;
+  @Output() headerText = new EventEmitter<string>();
+
+  title = 'Angular 2Do';
+  subTitle = 'Educational Angular project.';
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.testHeaderTitle);
+  }
+
+  testEvent(): void {
+    this.headerText.emit(`hello! It's header.`);
+
+  }
+
+
+}
+
