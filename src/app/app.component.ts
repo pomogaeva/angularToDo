f4dd66ece6e818d7062319947c883ea009b29e7c
Tetiana Pomohaieva <t.pomogaeva@gmail.com>
2020-12-11 20:21:09 +0200

    updated compontents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-todo';
+
+  appHead(event: string): void {
+    console.log(event);
+
+  }
 }
+
---
+++ src/app/core/interfaces/todo.interface.ts
@@ -0,0 +1,8 @@
+export default interface Todo {
+  id: number;
+  title: string;
+  description: string;
+  isDone: boolean;
+}
+
+// export { Todo as TodoItem };
