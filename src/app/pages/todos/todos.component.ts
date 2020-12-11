6a4858e3e5f931a0f803054623f78c314a97222e
Tetiana Pomohaieva <t.pomogaeva@gmail.com>
2020-12-11 21:08:02 +0200

    update proj

--- src/app/pages/todos/todos.component.ts
+++ src/app/pages/todos/todos.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/core/interfaces';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss']
 })
+
 export class TodosComponent implements OnInit {
+  todoList: Array<Todo>;
 
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.todoList = TODO_DATA;
+    }, 2000);
+    //this.todoList = TODO_DATA;
   }
 
 }
+
+const TODO_DATA = [{
+  id: 1,
+  title: 'learn JS',
+  description: '',
+  isDone: true
+},
+{
+  id: 2,
+  title: 'learn Angular',
+  description: 'Test description text',
+  isDone: false
+}
+]
